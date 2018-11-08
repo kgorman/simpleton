fd2fdea76c95768fa6ce5ea7bfb438ae52a72480
kgorman <kgorman@me.com>
2018-11-08 16:21:08 -0600

    foo

--- src/main/java/io/eventador/FlinkReadWriteKafka.java
+++ src/main/java/io/eventador/FlinkReadWriteKafka.java
@@ -5,8 +5,7 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 
 public class FlinkReadWriteKafka {
     public static void main(String[] args) throws Exception {
@@ -26,19 +25,12 @@ public class FlinkReadWriteKafka {
         DataStream<String> messageStream = env
                 .addSource(new FlinkKafkaConsumer011<>(
                         params.getRequired("read-topic"),
-                        new SimpleStringSchema(),
+                        new JSONKeyValueDeserializationSchema(),
                         params.getProperties()));
-         // Print Kafka messages to stdout - will be visible in logs
-        messageStream.print();
 
         // If you want to perform some transformations before writing the data
         // back to Kafka, do it here!
-
-        // Write payload back to Kafka topic
-        messageStream.addSink(new FlinkKafkaProducer011<>(
-                    params.getRequired("write-topic"),
-                    new SimpleStringSchema(),
-                    params.getProperties())).name("Write To Kafka");
+        messageStream.print();
 
         env.execute("FlinkReadWriteKafka");
     }

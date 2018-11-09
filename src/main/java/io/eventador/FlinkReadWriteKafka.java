a60966da27ec4524f952b9cdf2599438c89f597c
kgorman <kgorman@me.com>
2018-11-09 13:05:20 -0600

    adding new tablesource

--- src/main/java/io/eventador/FlinkReadWriteKafka.java
+++ src/main/java/io/eventador/FlinkReadWriteKafka.java
@@ -1,14 +1,13 @@
 package io.eventador;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.table.descriptors.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
-import org.apache.flink.streaming.util.serialization.SerializationSchema;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.StreamTableEnvironment;
 
 public class FlinkReadWriteKafka {
     public static void main(String[] args) throws Exception {
@@ -25,23 +24,48 @@ public class FlinkReadWriteKafka {
         env.enableCheckpointing(300000); // 300 seconds
         env.getConfig().setGlobalJobParameters(params);
 
-        DataStream<ObjectNode> messageStream = env
-                .addSource(new FlinkKafkaConsumer011<>(
-                        params.getRequired("read-topic"),
-                        new JSONKeyValueDeserializationSchema(true),
-                        params.getProperties()));
+        StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
 
-        messageStream.print();
+        tableEnv
+                .connect(
+                        new Kafka()
+                                .version("0.11")
+                                .topic("kg_input")
+                                .startFromEarliest()
+                                .property("bootstrap.servers", "7f421e3e-kafka0.pub.va.eventador.io:9092")
+                )
+                .withFormat(
+                        new Json()
+                                // or by using a JSON schema which parses to DECIMAL and TIMESTAMP
+                                .jsonSchema(
+                                    "{" +
+                                    "  type: 'object'," +
+                                    "  properties: {" +
+                                    "    sensor: {" +
+                                    "      type: 'string'," +
+                                    "    }," +
+                                    "    temp: {" +
+                                    "      type: 'number'" +
+                                    "    }" +
+                                    "  }" +
+                                    "}"
+                                )
 
-        messageStream.addSink(new FlinkKafkaProducer011<>(
-                params.getRequired("write-topic"),
-                new SerializationSchema<ObjectNode>() {
-                    @Override
-                    public byte[] serialize( ObjectNode element ) {
-                        return element.toString().getBytes();
-                    }
-                },
-                params.getProperties())).name("Write To Kafka");
+                )
+                .withSchema(
+                        new Schema()
+                                .field("rowtime", Types.SQL_TIMESTAMP)
+                                .proctime()
+                                .field("sensor", Types.STRING)
+                                .field("temp", Types.LONG)
+                )
+                .inAppendMode()
+                .registerTableSource("mySourceTable");
+
+
+        Table result2 = tableEnv.sqlQuery("SELECT * FROM mySourceTable WHERE temp > 10");
+        String ds = result2.toString();
+        System.out.println(ds);
 
         env.execute("FlinkReadWriteKafka");
     }
